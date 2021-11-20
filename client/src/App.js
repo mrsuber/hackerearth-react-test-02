2f67fb8cc48d6975e1b65a5c3fe7ff6513b0adac
mohamad <msiysinyuy@gmail.com>
2021-11-20 11:18:16 +0100

    initial commit after creating react app and cleaning

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import {Table} from './components'
+function App() {
+  return (
+    <div>
+        <Table/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/table/Table.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Table = () => {
+
+  return (
+    <div >
+
+    table component here
+
+
+    </div>
+  )
+}
+
+export default Table
